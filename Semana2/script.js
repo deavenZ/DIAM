bf16c3e77a59ed9ee21d3a0ff56ca456a3849faa
deavenZ <playerjony04@gmail.com>
2025-02-23 13:35:11 +0000

    last update for semana 2

--- Semana2/script.js
+++ Semana2/script.js
@@ -15,25 +15,72 @@ document.addEventListener("DOMContentLoaded", function () {
     hideImage();
 });
 
+var submitEnabled = false;
+
+function verifySubmit() {
+
+    var commentSection = document.getElementById('comment');
+    var warningMessage = document.getElementById('commentWarning');
+
+    if (commentSection.value.trim() !== ""){
+        if(!submitEnabled) {
+            commentSection.style.border = "2px solid red"; 
+            warningMessage.innerText = `Ainda não verificou o comentário!`;
+            warningMessage.style.display = "block";
+            return false
+        }
+    }
+    commentSection.style.border = "";
+    warningMessage.style.display = "none";
+    return true;
+}
+
 function verifyComment() {
 
-    const palavrasmas = [
+    const palavrasMas = [
         "abecula", "abentesma", "achavascado", "alimaria", "andrajoso", "barrega", "biltre",
         "cacostomo", "cuarra", "estolido", "estroso", "estultiloquio", "nefelibata", "nescio", 
         "pechenga", "sevandija", "somitico", "tatibitate", "xexe", "cheche", "xexelento"
     ];
 
     var commentSection = document.getElementById('comment');
+    var warningMessage = document.getElementById('commentWarning');
 
     var commentText = commentSection.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-
-    console.log("Got: " + commentText);
 
     for (const palavra of palavrasMas) {
         if (commentText.includes(palavra)) {
             commentSection.value = "";
-            alert("Não pode utilizar a palavra " + palavra);
+            commentSection.style.border = "2px solid red";  
+            warningMessage.innerText = `A palavra "${palavra}" não é permitida!`;
+            warningMessage.style.display = "block";
+            submitEnabled = false;
             return;
         }
     }
+    commentSection.style.border = "";
+    warningMessage.style.display = "none";
+    submitEnabled = true;    
+}
+
+var imgId = 1
+
+function slideshow(nextOrBefore) {
+
+    var imageSlide = document.getElementById('imageSlide');
+
+    if(nextOrBefore == 1) {
+        if(imgId !== 3) {
+            imgId++;
+        } else {
+            imgId = 1;
+        }
+    } else {
+        if(imgId !== 1) {
+            imgId--;
+        } else {
+            imgId = 3;
+        }
+    }
+    imageSlide.src = "vmfoto" + imgId + ".jpg";
 }
