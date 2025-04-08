8e72fd823e7c8cbe184008cadf0156f9ee33a77a
rafalopss <rmlss1@iscte-iul.pt>
2025-04-08 14:56:04 +0100

    5) done, QuestionTable to do

---
+++ Semana8/sitepr/votacao-frontend/src/Header.js
@@ -0,0 +1,20 @@
+function Header() {
+    return (
+        <>
+            <div className="text-center">
+                <img
+                    src="/iscte.jpg"
+                    width="600"
+                    alt="ISCTE"
+                    className="img-thumbnail"
+                    style={{ marginTop: "20px" }}
+                />
+                <h2>Exemplo</h2>
+                <h3>Integração de Django com React</h3>
+            </div>
+        </>
+    );
+}
+
+
+export default Header;
